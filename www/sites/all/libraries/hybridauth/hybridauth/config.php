da75227563a19f14b6a04e373dfdcd0fff0342ee
sagneboucar <tiagoboucar@yahoo.fr>
2015-08-31 17:35:45 +0000

    update hybridauth settings

--- www/sites/all/libraries/hybridauth/hybridauth/config.php
+++ www/sites/all/libraries/hybridauth/hybridauth/config.php
@@ -1,72 +1,72 @@
 <?php
-/*!
+/**
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+* (c) 2009-2015, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
 */
 
 // ----------------------------------------------------------------------------------------
 //	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
 // ----------------------------------------------------------------------------------------
 
-return 
+return
 	array(
-		"base_url" => "http://localhost/hybridauth-git/hybridauth/", 
+		"base_url" => "http://localhost/hybridauth-git/hybridauth/",
 
-		"providers" => array ( 
+		"providers" => array (
 			// openid providers
 			"OpenID" => array (
 				"enabled" => true
 			),
 
-			"Yahoo" => array ( 
+			"Yahoo" => array (
+				"enabled" => true,
+				"keys"    => array ( "key" => "", "secret" => "" ),
+			),
+
+			"AOL"  => array (
+				"enabled" => true
+			),
+
+			"Google" => array (
 				"enabled" => true,
 				"keys"    => array ( "id" => "", "secret" => "" ),
 			),
 
-			"AOL"  => array ( 
-				"enabled" => true 
+			"Facebook" => array (
+				"enabled" => true,
+				"keys"    => array ( "id" => "", "secret" => "" ),
+				"trustForwarded" => false
 			),
 
-			"Google" => array ( 
+			"Twitter" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ), 
-			),
-
-			"Facebook" => array ( 
-				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ), 
-			),
-
-			"Twitter" => array ( 
-				"enabled" => true,
-				"keys"    => array ( "key" => "", "secret" => "" ) 
+				"keys"    => array ( "key" => "", "secret" => "" )
 			),
 
 			// windows live
-			"Live" => array ( 
+			"Live" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ) 
+				"keys"    => array ( "id" => "", "secret" => "" )
 			),
 
-			"MySpace" => array ( 
+			"LinkedIn" => array (
 				"enabled" => true,
-				"keys"    => array ( "key" => "", "secret" => "" ) 
-			),
-
-			"LinkedIn" => array ( 
-				"enabled" => true,
-				"keys"    => array ( "key" => "", "secret" => "" ) 
+				"keys"    => array ( "key" => "", "secret" => "" )
 			),
 
 			"Foursquare" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ) 
+				"keys"    => array ( "id" => "", "secret" => "" )
 			),
 		),
 
-		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+		// If you want to enable logging, set 'debug_mode' to true.
+		// You can also set it to
+		// - "error" To log only error messages. Useful in production
+		// - "info" To log info and error messages (ignore debug messages)
 		"debug_mode" => false,
 
+		// Path to file writable by the web server. Required if 'debug_mode' is not false
 		"debug_file" => "",
 	);
